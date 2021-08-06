29b78aa1b2e390f655b5a8f0669ba461f690d649
Shafqat Hasan <60235150+shafhasan@users.noreply.github.com>
2021-08-07 05:21:39 +0600

    Long Day

---
+++ app.js
@@ -0,0 +1,43 @@
+//Agent slides
+const agentSlides = n => showAgentSlides(agentSlideIndex += n);
+
+// const currentSlide = n => showAgentSlides(agentSlideIndex = n);
+
+const showAgentSlides = n => {
+  const slides = document.getElementsByClassName('agents');
+  if (n > slides.length) {
+    agentSlideIndex = 1;
+  }
+  if (n < 1) {
+    agentSlideIndex = slides.length;
+  }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[agentSlideIndex - 1].style.display = "block";
+}
+
+let agentSlideIndex = 1;
+showAgentSlides(agentSlideIndex);
+
+//Gun slides
+const gunSlides = n => showGunSlides(gunSlideIndex += n);
+
+// const currentSlide = n => showGunSlides(gunSlideIndex = n);
+
+const showGunSlides = n => {
+  const slides = document.getElementsByClassName('guns');
+  if (n > slides.length) {
+    gunSlideIndex = 1;
+  }
+  if (n < 1) {
+    gunSlideIndex = slides.length;
+  }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[gunSlideIndex - 1].style.display = "block";
+}
+
+let gunSlideIndex = 1;
+showGunSlides(gunSlideIndex);
